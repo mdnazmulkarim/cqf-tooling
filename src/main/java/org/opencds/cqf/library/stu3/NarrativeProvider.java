83c8c3d9dd2da001da576b26b406002769dbcf4e
Chris Schuler <cschuler@myharmoniq.com>
2019-12-12 13:39:20 -0700

    Continuing to integrate Ruler measure generation logic

--- src/main/java/org/opencds/cqf/library/stu3/NarrativeProvider.java
+++ src/main/java/org/opencds/cqf/library/stu3/NarrativeProvider.java
@@ -1,14 +1,22 @@
 package org.opencds.cqf.library.stu3;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.narrative.CustomThymeleafNarrativeGenerator;
 import org.hl7.fhir.dstu3.model.Narrative;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.opencds.cqf.library.BaseNarrativeProvider;
 
+import java.util.Objects;
+
 public class NarrativeProvider extends BaseNarrativeProvider<Narrative> {
 
     public NarrativeProvider() {
-        super(NarrativeProvider.class.getClassLoader().getResource("narratives/stu3/narrative.properties").toString());
+        super(Objects.requireNonNull(NarrativeProvider.class.getClassLoader().getResource("narratives/stu3/narrative.properties")).toString());
+    }
+
+    public NarrativeProvider(String pathToPropertiesFile)
+    {
+        super(pathToPropertiesFile);
     }
 
     public Narrative getNarrative(FhirContext context, IBaseResource resource) {
